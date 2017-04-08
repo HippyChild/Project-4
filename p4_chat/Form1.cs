5e00c9bc269c90e731606ad4614fc4b521c30e34
Tyler <tylerhebert51@yahoo.com>
2017-04-08 17:04:52 -0500

    Early server protocol added.

--- p4_chat/Form1.cs
+++ p4_chat/Form1.cs
@@ -8,13 +8,40 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
 namespace p4_chat
 {
     public partial class Form1 : Form
     {
+        private void Server()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 51111);
+            listener.Start();
+            while (true)
+            {
+                try
+                {
+
+                }catch(Exception e)
+                {
+                    Console.WriteLine("Error: " + e);
+                    Console.ReadLine();
+                }
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Thread(Server).Start();
         }
     }
 }
